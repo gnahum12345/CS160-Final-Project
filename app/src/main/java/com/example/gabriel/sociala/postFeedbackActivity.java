0c54fdb176960d0075fdeb3cb8fb2fe402b8088c
v_canshen <v_canshen@tencent.com>
2019-11-26 00:41:17 -0800

    finished post feedback activity

--- app/src/main/java/com/example/gabriel/sociala/postFeedbackActivity.java
+++ app/src/main/java/com/example/gabriel/sociala/postFeedbackActivity.java
@@ -1,5 +1,6 @@
 package com.example.gabriel.sociala;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,27 +9,22 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class editFeedbackActivity extends AppCompatActivity {
+public class postFeedbackActivity extends AppCompatActivity {
 
     ImageView postImageView;
-    Button backButton, nextButton;
-    Button filterButton, tagButton, stickerButton, musicButton;
+    Button backButton;
     ImageView profilePic;
     TextView userName;
     EditText caption, reason;
+    Button addVideoButton, postFeedbackButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_feedback);
+        setContentView(R.layout.activity_post_feedback);
 
         postImageView = findViewById(R.id.image_view_photo);
         backButton = findViewById(R.id.back_button);
-        nextButton = findViewById(R.id.next_button);
-        filterButton = findViewById(R.id.filter_button);
-        tagButton = findViewById(R.id.tag_button);
-        stickerButton = findViewById(R.id.sticker_button);
-        musicButton = findViewById(R.id.music_button);
 
         userName = findViewById(R.id.textView_username);
         profilePic = findViewById(R.id.profile_image);
@@ -36,28 +32,42 @@ public class editFeedbackActivity extends AppCompatActivity {
         caption = findViewById(R.id.editText_caption);
         reason = findViewById(R.id.editText_reason);
 
+        addVideoButton = findViewById(R.id.add_video_button);
+        postFeedbackButton = findViewById(R.id.post_feedback_button);
+
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO: back to previous page.
-                finish();
+                backToEdit();
             }
         });
 
-        nextButton.setOnClickListener(new View.OnClickListener() {
+        postFeedbackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                postFeedback();
             }
         });
 
-        filterButton.setOnClickListener(new View.OnClickListener() {
+        addVideoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO: add image processing api.
-                finish();
+                addVideo();
             }
         });
 
+    }
+
+    public void backToEdit() {
+        Intent intent = new Intent(this, editFeedbackActivity.class);
+        startActivity(intent);
+    }
+
+    public void postFeedback() {
+        // TODO: add to database and navigate to the post.
+    }
+
+    public void addVideo() {
+        // TODO: add the video to database and display it on post page.
     }
 }
