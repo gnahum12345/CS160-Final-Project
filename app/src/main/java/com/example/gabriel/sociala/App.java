5c176bd97dd10c0edfb2c217e56d1e0756e9abb1
Zachary Chen <zacharyczf@gmail.com>
2019-11-26 21:41:43 -0800

    remove ParseUser.logOut()

--- app/src/main/java/com/example/gabriel/sociala/App.java
+++ app/src/main/java/com/example/gabriel/sociala/App.java
@@ -40,7 +40,5 @@ public class App extends Application {
                 .applicationId("ucb-social-a") // should correspond to APP_ID env variable
                 .clientKey("ucb-social-a-final-project")
                 .server("https://ucb-social-a.herokuapp.com/parse/").build());
-
-        ParseUser.logOut();
     }
 }
